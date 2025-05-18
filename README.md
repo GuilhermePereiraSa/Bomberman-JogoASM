d805b5bea27d2185e86d4d736050a32ccd7487c1
GuilhermePereiraSa <guilherme1478.wrath@gmail.com>
2025-05-18 19:40:54 -0300

    start

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Bomberman-JogoASM
+# 💣 Bomberman-JogoASM
+
+Uma versão do clássico **Bomberman**, desenvolvida em **Assembly**!  
+Este projeto foi criado com fins educacionais, explorando conceitos de baixo nível e manipulação direta de hardware.
+
+---
+
+## 🎮 Sobre o Jogo
+
+- Controle seu personagem e coloque bombas para eliminar obstáculos ou inimigos.
+- Evite ser pego pelas explosões ou encurralado.
+- Divirta-se com uma versão retrô de um clássico, agora em Assembly!
+
+---
+
+## 🚀 Como Rodar
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/Bomberman-JogoASM.git
+   cd Bomberman-JogoASM
+
+2. Acesse o site Giroto Emulator:
+   https://proc.giroto.dev/
+
+3. Clique em "Files" e faça o upload dos arquivos do projeto.
+
+4. Vá para a aba "State" e clique em "Run" para iniciar o jogo.
+
+
+## Créditos: 
+- Eduardo Simões, https://github.com/simoesusp/Processador-ICMC/blob/master/Software_Assembly/Nave11.asm
+
--- bomberman/Bomberman.asm
+++ bomberman/Bomberman.asm
@@ -1,25 +1,132 @@
-; Tela de Menu
+; Tela 
 ; carrega tela de menu
 
+jmp menu
+
+Msg1: string "DESEJA JOGAR NOVAMENTE? (s/n)" ; Mensagem para jogar novamente
+OpSaidaFinal: var #1		; Contem a letra que foi digitada - 1 byte apenas
+MsgFim: string "OBRIGADO POR JOGAR!"
+
+
+MsgTitulo: string "==== BOMBERMAN ===="
+MsgOpcao1: string "1 - JOGAR"
+MsgOpcao2: string "2 - SAIR"
+OpInicio: var #1 ; guarda a escolha do jogador
+
+
+;; tecla == 'n'
+    ;call DigLetra
+	;loadn r0, #'n'
+	;load r1, Letra
+	;cmp r0, r1
+;
+    ;; tecla == 's'
+    ;loadn r0, #'s'
+	;cmp r0, r1
 
 
 
-;iniciar jogo quando apertado a tecla
-; Jogo em si
+; copiar loop
 
-INCHAR r1 ; ler tecla em R1
+; copiar movimentação e suas funções
 
-CMP r1, #SETARY ; compara com codigo de seta para cima
-JEQ movCima ; se igual, salta para rot. de mover para cima
+	
+;********************************************************
+;                       MENU
+;********************************************************
 
-CMP r1, #BOMBA ; tecla de bomba
-JEQ soltarBomba
+menu:
+    call ApagaTela
 
-JMP loopInicio ; volta para o inicio do loop
+    ; Exibir titulo
+    loadn r0, #526 ; posicao na tela
+    loadn r1, #MsgTitulo
+    loadn r2, #0
+    call ImprimeStr
+
+    ; Exibir Opcao 1
+    loadn r0, #605 ; posicao na tela
+    loadn r1, #MsgOpcao1
+    loadn r2, #0
+    ;call ImprimeStr
+
+    ; Exibir Opcao 2
+    loadn r0, #605 ; posicao na tela
+    loadn r1, #MsgOpcao2
+    loadn r2, #0
+    call ImprimeStr
+
+    ; provavel ter que fazer o numero virar char!!!
+    inchar r1
+    store r1, OpInicio
+
+    ; Verifica opcao
+    loadn r2, #'1'
+    cmp r1, r2
+    je Jogar 
+
+    loadn r2, #'2'
+    cmp r1, r2
+    je Sair 
+
+    ; Se invalido, volta ao menu
+
+    jmp menu
+
+;------------------------
+	
+;********************************************************
+;                       JOGAR
+;********************************************************
+Jogar:
+
+
+;------------------------
+
+	
+;********************************************************
+;                       SAIR
+;********************************************************
+Sair:
+    call ApagaTela
+    ; Pode colocar mensagem de despedida, ou parar o programa
+    loadn r0, #100
+    loads r1, MsgFim
+    
+    ;call ImprimeStr
+
+    hlt ; halt 
 
 
 
-movCima:
+    
+
+
+
+
+;------------------------
+
+	
+;********************************************************
+;                       APAGA TELA
+;********************************************************
+ApagaTela:
+	push r0
+	push r1
+	
+	loadn r0, #1200		; apaga as 1200 posicoes da Tela
+	loadn r1, #' '		; com "espaco"
+	
+	   ApagaTela_Loop:	;;label for(r0=1200;r3>0;r3--)
+		dec r0
+		outchar r1, r0
+		jnz ApagaTela_Loop
+ 
+	pop r1
+	pop r0
+	rts	
+	
+;------------------------	
 
 
 ; Fim do jogo
